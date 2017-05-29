f79343eefdeee034518699bcfe57de8abea21e6a
Brandon D'Souza <brandondsz26@gmail.com>
2017-05-29 14:55:33 +0530

    Updated files that were missed

--- MovieDb/Scripts/movieDb/actor.js
+++ MovieDb/Scripts/movieDb/actor.js
@@ -1,5 +1,22 @@
-﻿function OnActorPostSuccess(data) {
+﻿function onActorPostSuccess(data) {
     if (data !== null && typeof data === "object" && data.success !== undefined && data.success === true) {
         window.location = $("#DisplayActorUrl").val() + "?actorId=" + data.actorId;
     }
+}
+
+function onAdditionalData() {
+    return {
+        text: $("#actorSearch").val()
+    };
+}
+
+function onActorSelect(e) {
+    var dataItem = this.dataItem(e.item.index());
+    window.location = $("#DisplayActorUrl").val() + "?actorId=" + dataItem.RowId;
+}
+
+function onActorGridChange(e) {
+    grid = e.sender;
+    var currentDataItem = grid.dataItem(this.select());
+    window.location = $("#DisplayActorUrl").val() + "?actorId=" + currentDataItem.RowId;
 }
--- MovieDb/Scripts/movieDb/producer.js
+++ MovieDb/Scripts/movieDb/producer.js
@@ -1,5 +1,23 @@
-﻿function OnProducerPostSuccess(data) {
+﻿function onProducerPostSuccess(data) {
     if (data !== null && typeof data === "object" && data.success !== undefined && data.success === true) {
         window.location = $("#DisplayProducerUrl").val() + "?producerId=" + data.producerId;
     }
+}
+
+
+function onAdditionalData() {
+    return {
+        text: $("#producerSearch").val()
+    };
+}
+
+function onProducerSelect(e) {
+    var dataItem = this.dataItem(e.item.index());debugger
+    window.location = $("#DisplayProducerUrl").val() + "?producerId=" + dataItem.RowId;
+}
+
+function onProducerGridChange(e) {
+    grid = e.sender;
+    var currentDataItem = grid.dataItem(this.select());
+    window.location = $("#DisplayProducerUrl").val() + "?producerId=" + currentDataItem.RowId;
 }
